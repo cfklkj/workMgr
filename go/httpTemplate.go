97676b6c9c55d0194b3b37f93d20755d2b369beb
DESKTOP-255E2LD\weile <luokangliang@weile.com>
2018-11-02 13:57:43 +0800

    map  mutex

--- go/httpTemplate.go
+++ go/httpTemplate.go
@@ -7,18 +7,68 @@ import(
 	"text/template" 
 	"errors"	
     "bytes" 
+    "io" 
   //  "strings"
     "io/ioutil"
     "encoding/json" 
+    "sync" 
 )  
 type Callback func (data []byte)
 
-type SendRequest struct{
-    ResId int
+type SendRequest struct{ 
+    MutexId int
     Url string
     Body string
 }
-var m_mapRes map[int]http.ResponseWriter = map[int]http.ResponseWriter{}
+
+type MutexInfo struct{ 
+    isLock bool
+    mutexs sync.Mutex 
+}
+
+ 
+var g_Mutex [108]MutexInfo 
+var g_mapMutex map[int]MutexInfo
+var mutexIndex int
+var g_redisBackStr string
+
+
+func mapMutexInit(){ 
+    index := 0
+    g_mapMutex = make(map[int]MutexInfo)
+    for   index < 100 {    
+        g_mapMutex[index] = g_Mutex[index]
+        index += 1
+    } 
+
+}
+
+func mapMutexLock(id int){  
+    for k, v := range(g_mapMutex) {
+        if(k == id) {
+            if( !v.isLock ){  
+                v.isLock = true
+                v.mutexs.Lock()
+                break
+            }
+            
+        }
+    } 
+}
+func mapMutexUnlock(id int){ 
+    for k, v := range(g_mapMutex) {
+        if(k == id) {
+            if( v.isLock ){ 
+                v.isLock = false
+                v.mutexs.Unlock()
+                break
+            }
+            
+        }
+    } 
+}
+
+ var g_waitEvent sync.WaitGroup
 
 func WriteTemplateToHttpResponse(res http.ResponseWriter, t *template.Template) error {
     if t == nil || res == nil {
@@ -32,10 +82,6 @@ func WriteTemplateToHttpResponse(res http.ResponseWriter, t *template.Template) 
     res.Header().Set("Content-Type", "text/html; charset=utf-8")
     _, err = res.Write(buf.Bytes())
     return err
-}
-
-func sendBackBrowser(data []byte){
-
 }
 
 func HomePage(res http.ResponseWriter, req *http.Request) {
@@ -55,16 +101,17 @@ func HomePage(res http.ResponseWriter, req *http.Request) {
             fmt.Println(err)
             return
         }
-    } else if req.Method == "POST" {
+    } else if req.Method == "POST" { 
       /*  if strings.Contains(req.URL.Path, "/Explorer") { 
             OnExplorer(res, req)
         }else if strings.Contains(req.URL.Path, "/Workbook"){
             OnWorkbook(res, req)
         }else{ 
             OnDBMgr(res,req)
-        }*/         
-        var data SendRequest
-        data.ResId = mapBuff(res)
+        }*/       
+        var data SendRequest   
+        data.MutexId = mutexIndex + 1    
+        mutexIndex =  data.MutexId % 100
         data.Url = req.URL.Path
         body, _ := ioutil.ReadAll(req.Body) 
         data.Body = string(body)
@@ -72,14 +119,21 @@ func HomePage(res http.ResponseWriter, req *http.Request) {
         if(err != nil){
             return
         }
-        redisSend(jsonData)
+        fmt.Printf("id-%d\n", data.MutexId)
+       // mapMutexLock(data.MutexId)
+        if(!redisSend(jsonData)){
+            g_redisBackStr = "c++ redis miss"  
+            //mapMutexUnlock(data.MutexId)
+        }else
+        {
+            g_waitEvent.Add(1)
+            g_waitEvent.Wait()
+        } 
+        fmt.Printf("web send info %d--%s\n",data.MutexId, g_redisBackStr)
+        io.WriteString(res, g_redisBackStr) 
     }    
 }
-
-func mapBuff(res http.ResponseWriter) int{
-    m_mapRes[0] = res
-    return 0
-}
+ 
 
 func OnAjax(res http.ResponseWriter, req *http.Request) { 
     if req.Method == "GET" {   
@@ -96,5 +150,20 @@ func OnAjax(res http.ResponseWriter, req *http.Request) {
     } else if req.Method == "POST" {
         ReadMsg(res,req)
     }    
+    OnExplorer(res, req)
     OnDBMgr(res,req)
 } 
+func sendBackBrowser(data []byte){
+    var dataValue SendRequest 
+    err := json.Unmarshal(data, &dataValue) 
+    if(err != nil){ 
+        g_redisBackStr = "error info"
+    } else
+    { 
+        g_redisBackStr = dataValue.Body;
+    }
+    fmt.Printf("get redis info %d--%s\n",dataValue.MutexId, g_redisBackStr)
+    //mapMutexUnlock(dataValue.MutexId)
+    g_waitEvent.Done()
+}
+
